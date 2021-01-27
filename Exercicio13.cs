1f9c17d6e62d96de459df06ddec2ee70a4a4fcf7
Jean Lourenco <jean.lourenco@aluno.unip.br>
2021-01-27 19:50:15 -0300

    Adicionado exercício 1.3 e pequenas correções no 1.2.

---
+++ Exercicio13.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio13
+{
+    class Exercicio13
+    {
+        public int ContarNumerosPrimos(int n)
+        {
+            int[] numeros = new int[n];
+            List<int> primos = new List<int>();
+            int qnt_primos = 0;
+            for (int i = 0; i <= n - 1; i++)
+            {
+                numeros[i] = n - i;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (numeros[i] == 2 || numeros[i] == 5 || numeros[i] == 3 || numeros[i] == 7)
+                {
+                    primos.Add(numeros[i]);
+                }
+                else if (numeros[i] > 1 && numeros[i]%2 != 0 && numeros[i]%5 != 0 && numeros[i]%3 != 0 && numeros[i]%7 != 0)
+                {
+                    primos.Add(numeros[i]);
+                }
+            }
+            qnt_primos = primos.Count;
+            return qnt_primos;
+        }
+    }
+}
--- MainFunc.cs
+++ MainFunc.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Collections.Generic;
 using Exercicio11;
 using Exercicio12;
+using Exercicio13;
 
 namespace MainFunc
 {
@@ -9,15 +11,17 @@ namespace MainFunc
     {
         static void Main()
         {
-            Exercicio12.Exercicio12 p = new Exercicio12.Exercicio12();
+            /*Exercicio12.Exercicio12 p = new Exercicio12.Exercicio12();
             Debug.Assert(p.CalcularPremio(100, "basic", null) == 100.00);
             Debug.Assert(p.CalcularPremio(100, "vip", null) == 120.00);
             Debug.Assert(p.CalcularPremio(100, "premium", null) == 150.00);
             Debug.Assert(p.CalcularPremio(100, "special", null) == 200.00);
             Debug.Assert(p.CalcularPremio(100, "basic", 1.2) == 120.00);
             Debug.Assert(p.CalcularPremio(100, "basic", 1.4) == 140.00);
-            Debug.Assert(p.CalcularPremio(100, "vip", 1.5) == 150.00);
-            Console.WriteLine("Aqui");
+            Debug.Assert(p.CalcularPremio(100, "vip", 1.5) == 150.00);*/
+
+            Exercicio13.Exercicio13 e13 = new Exercicio13.Exercicio13();
+            Console.WriteLine(e13.ContarNumerosPrimos(20));
         }
     }
 }
