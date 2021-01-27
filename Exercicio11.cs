ee0dfc6f5626384c7ca29dcde224fdaada6d9355
Jean Lourenco <jean.lourenco@aluno.unip.br>
2021-01-27 18:16:54 -0300

    Alteração importante.

---
+++ Exercicio11.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace luby_incubadora11
+{
+    class Exercicio11
+    {
+        public int CalcularFatorial(int n)
+        {
+            int[] fatoriais = new int[n];
+            int fatorial = n;
+            for (int i = 0; i <= n - 1; i++)
+            {
+                fatoriais[i] = n - i;
+            }
+            for (int i = 1; i < n - 1; i++)
+            {
+                fatorial *= fatoriais[i];
+            }
+            return fatorial;
+        }
+        static void Main()
+        {
+            Exercicio11 p = new Exercicio11();
+            Console.WriteLine(p.CalcularFatorial(5));
+        }
+    }
+}
