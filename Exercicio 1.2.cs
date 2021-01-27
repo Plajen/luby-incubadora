d56b71f8a9b723e0448e4601c83d61172c23e48f
Jean Lourenco <jean.lourenco@aluno.unip.br>
2021-01-27 17:53:52 -0300

    Implementados testes unitários.

--- Exercicio 1.2.cs
+++ Exercicio 1.2.cs
@@ -1,16 +1,16 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace luby_incubadora
 {
     class Program
     {
-
-        public void CalcularPremio(float valor_premio, string tipo_premio, float? param_proprio)
+        public double CalcularPremio(double valor_premio, string tipo_premio, double? param_proprio)
         {
             if (param_proprio != null && param_proprio > 0)
             {
-                valor_premio *= Convert.ToInt32(param_proprio);
-                Console.WriteLine("O valor calculado do prêmio é: {0}", valor_premio);
+                valor_premio *= (double)param_proprio;
+                return valor_premio;
             }
             else if (param_proprio == null)
             {
@@ -20,29 +20,35 @@ namespace luby_incubadora
                         valor_premio *= 1;
                         break;
                     case "vip":
-                        valor_premio *= 1.2f;
+                        valor_premio *= 1.2;
                         break;
                     case "premium":
-                        valor_premio *= 1.5f;
+                        valor_premio *= 1.5;
                         break;
                     case "deluxe":
-                        valor_premio *= 1.8f;
+                        valor_premio *= 1.8;
                         break;
                     case "special":
                         valor_premio *= 2;
                         break;
                 }
-                Console.WriteLine("O valor calculado do prêmio é: {0}", valor_premio);
+                return valor_premio;
             }
             else
             {
-                Console.WriteLine("Por favor, insira um parâmetro próprio válido (positivo e inteiro).");
+                return 0;
             }
         }
         static void Main()
         {
             Program p = new Program();
-            p.CalcularPremio(100,"basic",1.9f);
+            Debug.Assert(p.CalcularPremio(100, "basic", null) == 100.00);
+            Debug.Assert(p.CalcularPremio(100, "vip", null) == 120.00);
+            Debug.Assert(p.CalcularPremio(100, "premium", null) == 150.00);
+            Debug.Assert(p.CalcularPremio(100, "special", null) == 200.00);
+            Debug.Assert(p.CalcularPremio(100, "basic", 1.2) == 120.00);
+            Debug.Assert(p.CalcularPremio(100, "basic", 1.4) == 140.00);
+            Debug.Assert(p.CalcularPremio(100, "vip", 1.5) == 150.00);
         }
     }
 }
